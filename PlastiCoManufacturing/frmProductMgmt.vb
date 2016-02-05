5d03155fea64227a907cd37a468b502fdb441a28
tizz98 <dev.tizz98@gmail.com>
2016-02-04 18:59:57 -0800

    update list boxes

--- PlastiCoManufacturing/frmProductMgmt.vb
+++ PlastiCoManufacturing/frmProductMgmt.vb
@@ -22,9 +22,21 @@
         lstAllBasicMaterials.DataSource = BaseMaterial.getDisplayList(baseMaterials)
     End Sub
 
+    Private Sub updateSubAssemblyFromProduct(product As Product)
+        lstSubAsmInProduct.DataSource = SubAssembly.getDisplayList(product.subAssemblies)
+    End Sub
+
+    Private Sub updateMaterialFromSubAsm(subAsm As SubAssembly)
+        lstMaterialsInSubAsm.DataSource = BaseMaterial.getDisplayList(subAsm.baseMaterials)
+    End Sub
+
     Private Sub lstProducts_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstProducts.SelectedIndexChanged
         Dim product As Product = products(lstProducts.SelectedIndex)
+        updateSubAssemblyFromProduct(product)
+    End Sub
 
-        Debug.WriteLine(product.name)
+    Private Sub lstAllSubAsm_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstAllSubAsm.SelectedIndexChanged
+        Dim subAssembly As SubAssembly = subAssemblies(lstAllSubAsm.SelectedIndex)
+        updateMaterialFromSubAsm(subAssembly)
     End Sub
 End Class
