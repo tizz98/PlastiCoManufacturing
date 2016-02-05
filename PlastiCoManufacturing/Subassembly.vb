def839750009e468d09484166da26978cf9a9957
tizz98 <dev.tizz98@gmail.com>
2016-02-05 07:51:07 -0800

    subassembly docs

--- PlastiCoManufacturing/Subassembly.vb
+++ PlastiCoManufacturing/Subassembly.vb
@@ -1,24 +1,106 @@
-﻿Public Class SubAssembly
+﻿'------------------------------------------------------------
+'-                File Name: SubAssembly.vb                 -
+'-                 Part of Project: Assign4                 -
+'------------------------------------------------------------
+'-                Written By: Elijah Wilson                 -
+'-                  Written On: 02/05/2016                  -
+'------------------------------------------------------------
+'- File Purpose:                                            -
+'-                                                          -
+'- Contains the SubAssembly class.                          -
+'------------------------------------------------------------
+Public Class SubAssembly
     Public name As String
     Public baseMaterials As List(Of BaseMaterial)
 
     Public Const INVALID_SUBASM_ERROR_STR As String = "This name for a subassembly is invalid, please try a different name."
 
+    '------------------------------------------------------------
+    '-                   Subprogram Name: New                   -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 02/05/2016                  -
+    '------------------------------------------------------------
+    '- Subprogram Purpose:                                      -
+    '-                                                          -
+    '- Create a new SubAssembly object with the provided name   -
+    '- and an empty list of base materials.                     -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- name - Name to be used for the sub assembly              -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
     Public Sub New(name As String)
         Me.name = name
         Me.baseMaterials = New List(Of BaseMaterial)
     End Sub
 
+    '------------------------------------------------------------
+    '-                   Subprogram Name: New                   -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 02/05/2016                  -
+    '------------------------------------------------------------
+    '- Subprogram Purpose:                                      -
+    '-                                                          -
+    '- Create a new SubAssembly object from a provided name and -
+    '- list of base materials.                                  -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- name - Name to be used for the sub assembly              -
+    '- baseMaterials - List of materials to be used for the sub -
+    '-                 assembly                                 -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
     Public Sub New(name As String, baseMaterials As List(Of BaseMaterial))
         Me.name = name
         Me.baseMaterials = baseMaterials
     End Sub
 
+    '------------------------------------------------------------
+    '-               Subprogram Name: addMaterial               -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 02/05/2016                  -
+    '------------------------------------------------------------
+    '- Subprogram Purpose:                                      -
+    '-                                                          -
+    '- Add a new material to the list of materials, while       -
+    '- keeping the list of materials sorted.                    -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- material - BaseMaterial to add                           -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
     Public Sub addMaterial(material As BaseMaterial)
         baseMaterials.Add(material)
         baseMaterials = modFixtureData.sortMaterialList(baseMaterials)
     End Sub
 
+    '------------------------------------------------------------
+    '-             Subprogram Name: removeMaterial              -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 02/05/2016                  -
+    '------------------------------------------------------------
+    '- Subprogram Purpose:                                      -
+    '-                                                          -
+    '- Removes a new material to the list of materials, if it   -
+    '- exists in the list.                                      -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- materialName - Name of the material to remove            -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- index - The index of the materialName in the list of     -
+    '-         materials                                        -
+    '------------------------------------------------------------
     Public Sub removeMaterial(materialName As String)
         Dim index As Integer = baseMaterials.FindIndex(Function(m) m.name.ToLower() = materialName.ToLower())
 
@@ -27,6 +109,30 @@
         End If
     End Sub
 
+    '------------------------------------------------------------
+    '-              Function Name: getDisplayList               -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 02/05/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This function gets a list of strings based on the name   -
+    '- of each sub assembly in the provided list and sorts the  -
+    '- list.                                                    -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- subAssemblies - The sub assemblies to get the list from  -
+    '- sorted - Whether or not to sort the returned list of     -
+    '-          strings                                         -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- returnSubAsm - The list of strings that gets populated   -
+    '-                and eventually returned.                  -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- List(Of String) - A list of all the subassembly's names. -
+    '------------------------------------------------------------
     Public Shared Function getDisplayList(subAssemblies As List(Of SubAssembly),
                                           Optional sorted As Boolean = True) As List(Of String)
         Dim returnSubAsm As New List(Of String)
@@ -42,6 +148,31 @@
         Return returnSubAsm
     End Function
 
+    '------------------------------------------------------------
+    '-             Function Name: validateNewSubAsm             -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 02/05/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This function checks whether or not a proposed name for  -
+    '- a sub assembly would be valid among a current list of    -
+    '- sub assemblies.                                          -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- subAsms - Current list of sub assemblies                 -
+    '- newSubAsmName - The proposed new name for a sub assembly -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- index - The index of the proposed sub assembly name in   -
+    '-         the list of current sub assemblies               -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- Boolean - Whether or not the proposed new sub assembly   -
+    '-           name is valid among the current sub            -
+    '-           assemblies.                                    -
+    '------------------------------------------------------------
     Public Shared Function validateNewSubAsm(subAsms As List(Of SubAssembly), newSubAsmName As String) As Boolean
         ' "Hi" = "hi", so we make each name lowercase before comparing
         Dim index As Integer = subAsms.FindIndex(Function(s) s.name.ToLower() = newSubAsmName.ToLower())
